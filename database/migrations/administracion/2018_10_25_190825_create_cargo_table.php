cd74d688865489583b2f3540a7d19d5544769199
Unknown <cristianjojoa01@gmail.com>
2018-12-28 17:06:58 -0500

    estructura invitro

---
+++ database/migrations/administracion/2018_10_25_190825_create_cargo_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateCargoTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('cargo', function(Blueprint $table)
+		{
+			$table->integer('id_cargo', true);
+			$table->string('cargo', 45);
+		});
+	}
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('cargo');
+	}
+
+}
